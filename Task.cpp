f0a21ae14499cb7d6a64a385b1ce75037dd2ca5b
YourAverageSciencePal <40741641+YourAverageSciencePal@users.noreply.github.com>
2018-08-23 18:46:53 +0500

    Doubly Linked List Class Implementation

--- Task.cpp
+++ Task.cpp
@@ -1,214 +1,244 @@
+//doubly linked list
 #include <iostream>
 using namespace std;
 
 struct Node
 {
   int data;
-  Node*left;
-  Node*right;
+  Node*next;
+  Node*prev;
 };
-class BST
+class DLL
 {
 private:
-  Node*root;
-  Node*insert(Node*temp,int d);
-  Node*getNode();
-  void inorder(Node*temp);//LNR
-  Node*find(Node*temp,int d);
-  Node*findMin(Node*temp);
-  Node*findMax(Node*temp);
-  Node*remove(Node*temp,int d);
+  Node*head;
 public:
-  BST();
-  BST(int d);
+  DLL();
+  DLL(int d);
+  Node*getNode(int d);
   void insert(int d);
-  void display();
-  bool find(int d);
-  int findMin();
-  int findMax();
+  void print();
+  void reverse_print();
+  bool isPresent(int d);
+  void clear();
+  Node*getHead();
   void remove(int d);
+  void reverse();
+  DLL&operator=(DLL&D);//assignment operator overloading
+  bool isEmpty();
+  void swap(Node *a,Node*b);
+  ~DLL();
 };
-BST::BST()
+DLL::DLL()
 {
-  root = NULL;
+  head = NULL;
 }
-BST::BST(int d)
+DLL::DLL(int d)
 {
-  root = getNode();
-  root->data = d;
+  head = getNode(d);
 }
-Node* BST::insert(Node*temp,int d)
-{
-  if(!temp)
-  {
-    temp = getNode();
-    temp->data = d;
-  }
-  else if(temp->data>d)
-  {
-    temp->left = insert(temp->left,d);
-  }
-  else if(temp->data<d)
-  {
-    temp->right = insert(temp->right,d);
-  }
-  return temp;
-}
-Node*BST::getNode()
+Node*DLL::getNode(int d)
 {
   Node*temp = new Node;
-  temp->right = NULL;
-  temp->left = NULL;
+  temp->data = d;
+  temp->next = NULL;
+  temp->prev = NULL;
   return temp;
 }
-void BST::insert(int d)
+void DLL::insert(int d)
 {
-  root = insert(root,d);
-}
-void BST::inorder(Node*t)
-{
-  if(!t)
+  if(!head)
   {
-    return;
+    head = getNode(d);
   }
-  inorder(t->left);
-  cout<<t->data<<endl;
-  inorder(t->right);
-}
-void BST::display()
-{
-  inorder(root);
-}
-Node*BST::find(Node*temp,int d)
-{
-  if(!temp)
+  else if(!head->next)
   {
-    return NULL;
-  }
-  else if(d>temp->data)
-  {
-    find(temp->right,d);
-  }
-  else if(d<temp->data)
-  {
-    find(temp->left,d);
+    Node*temp = getNode(d);
+    head->next = temp;
+    temp->prev = head;
   }
   else
   {
-    return temp;
+    Node*temp = head;
+    while(temp->next)
+    {
+      temp = temp->next;
+    }
+    Node*temp2 = getNode(d);
+    temp->next = temp2;
+    temp2->prev = temp;
   }
 }
-bool BST::find(int d)
+void DLL::print()
 {
-  if(find(root,d))
+  if(head)
   {
-    return true;
+    Node*temp = head;
+    while(temp)
+    {
+      cout<<temp->data<<"->";
+      temp = temp->next;
+    }
   }
-  else
+}
+void DLL::reverse_print()
+{
+  if(head)
+  {
+    Node*temp = head;
+    while(temp->next)
+    {
+      temp = temp->next;
+    }
+    //Last node is reached
+    //Now using prev
+    while(temp)
+    {
+      cout<<temp->data<<"->";
+      temp = temp->prev;
+    }
+
+  }
+}
+bool DLL::isPresent(int d)
+{
+  Node*temp = head;
+  while(temp)
+  {
+    if(temp->data == d)
+    {
+      return true;
+    }
+    temp = temp->next;
+  }
+  return false;
+}
+Node*DLL::getHead()
+{
+  return head;
+}
+DLL &DLL::operator =(DLL&D)
+{
+  if(this == &D)
+  {
+    return *this;
+  }
+  this->clear();
+  Node*temp = D.getHead();
+  while(temp)
+  {
+    this->insert(temp->data);
+    temp = temp->next;
+  }
+}
+void DLL::clear()
+{
+  Node*temp = head;
+  while(temp)
+  {
+    temp = temp->next;
+    delete head;
+    head = temp;
+  }
+  head = NULL;
+}
+DLL::~DLL()
+{
+  clear();
+}
+void DLL::remove(int d)
+{
+  if(isPresent(d))
+  {
+    if(head->data==d&&head->next)
+    {
+      Node*temp = head;
+      temp = temp->next;
+      delete head;
+      temp->prev = NULL;
+      head = temp;
+    }
+    else if(head->data==d&&!head->next)
+    {
+      delete head;
+      head = NULL;
+    }
+    else
+    {
+      Node*temp = head;
+      while(true)
+      {
+        if(temp->data == d)
+        {
+          break;
+        }
+        temp = temp->next;
+      }
+      if(!temp->next)
+      {
+        Node*temp2 = temp;
+        temp2 = temp2->prev;
+        temp2->next = NULL;
+        delete temp;
+      }
+      else
+      {
+        Node*temp2 = temp->prev;
+        Node*temp3 = temp->next;
+        temp2->next = temp3;
+        temp3->prev = temp2;
+      }
+    }
+  }
+}
+bool DLL::isEmpty()
+{
+  if(head)
   {
     return false;
   }
+  return true;
 }
-int BST::findMin()
+void DLL::reverse()
 {
-  Node*temp = findMin(root);
-  return temp->data;
-}
-Node*BST::findMin(Node*temp)
-{
-  if(!temp)
+  if(head)
   {
-    return NULL;
-  }
-  else if(!temp->left)
-  {
-    return temp;
-  }
-  else
-  {
-    return findMin(temp->left);
-  }
-}
-Node*BST::findMax(Node*temp)
-{
-  if(!temp)
-  {
-    return NULL;
-  }
-  else if(!temp->right)
-  {
-    return temp;
-  }
-  else
-  {
-    return findMax(temp->right);
-  }
-}
-int BST::findMax()
-{
-  Node*temp = findMax(root);
-  return temp->data;
-}
-Node*BST::remove(Node*temp,int d)
-{
-  Node*t;
-  if(!temp)
-  {
-    return NULL;
-  }
-  else if(d<temp->data)
-  {
-    temp->left = remove(temp->left,d);
-  }
-  else if(d>temp->data)
-  {
-    temp->right = remove(temp->right,d);
-  }
-  else if(temp->right&&temp->left)
-  {
-    t = findMin(temp->right);
-    temp->data = t->data;
-    temp->right = remove(temp->right,temp->data);
-  }
-  else
-  {
-    t = temp;
-    if(!temp->right)
+    Node*temp = head;
+    Node*temp2 =head;
+    while(temp2->next)
     {
-      temp = temp->left;
+      temp2 = temp2->next;
+      Node*temp3 = temp->next;
+      temp->next = temp->prev;
+      temp->prev = temp3;
+      // swap(temp->next,temp->prev);
+      temp = temp2;
     }
-    else if(!temp->left)
-    {
-      temp = temp->right;
-    }
-    delete t;
+    // swap(temp2->next,temp2->prev)
+    Node*temp3 = temp2->next;
+    temp2->next = temp2->prev;
+    temp2->prev = temp3;
+    head = temp2;
   }
-  return temp;
 }
-void BST::remove(int d)
+void DLL::swap(Node*a,Node*b)
 {
-  remove(root,d);
+  Node*temp = a;
+  a = b;
+  b = temp;
 }
 
 int main()
 {
-  BST B(9);
-  B.insert(10);
-  B.insert(1);
-  B.insert(2);
-  B.insert(0);
-  B.insert(-10);
-  B.insert(-101);
-  B.insert(-102);
-  B.insert(999);
-  B.display();
-  // cout<<B.find(-10)<<endl;
-  // cout<<B.findMin()<<endl;
-  // cout<<B.findMax()<<endl;
-  // B.display();
-  B.remove(-10);
-  B.remove(9);
-  cout<<"After removing:"<<endl;
-  B.display();
-
+  DLL D;
+  for(int i=0;i<1000;i++)
+  {
+    D.insert(i);
+  }
+  // cout<<D.isPresent(1000)<<endl;
+  // D.clear();
+  // D.reverse_print();
+  D.reverse();
+  D.print();
+  return 0;
 }
